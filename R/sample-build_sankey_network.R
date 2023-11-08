40fc12f32527c5e3e41f058e7d70b4533805ca06
Marcela Ishihara <106243581+cell-ai@users.noreply.github.com>
2023-11-08 11:25:32 -0300

    Update sample-build_sankey_network.R

--- R/sample-build_sankey_network.R
+++ R/sample-build_sankey_network.R
@@ -1,4 +1,4 @@
-build_sankey_network <- function(my_report, taxRanks =  c("D","K","P","C","O","F","G","S"), maxn=10,
+build_sankey_network <- function(my_report, taxRanks =  c("D","K","P","C","O","F","G","S", "S1"), maxn=11,
 				 zoom = F, title = NULL,
 				 ...) {
     stopifnot("taxRank" %in% colnames(my_report))
